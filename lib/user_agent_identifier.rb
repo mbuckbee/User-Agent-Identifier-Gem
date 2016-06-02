027b2e3a4ac777c78cf00fd9d00efc02a481fa85
mbuckbee <michaelbuckbee@gmail.com>
2016-06-02 23:16:01 +0000

    Init

---
+++ lib/user_agent_identifier.rb
@@ -0,0 +1,17 @@
+# This file was automatically generated for Expedited Addons by APIMATIC v2.0 ( https://apimatic.io ) on 06/02/2016
+require 'openssl'
+require 'json'
+require 'unirest'
+
+# APIMATIC Helper Files
+require_relative 'user_agent_identifier/api_helper.rb'
+require_relative 'user_agent_identifier/api_exception.rb'
+require_relative 'user_agent_identifier/configuration.rb'
+require_relative 'user_agent_identifier/user_agent_identifier_client.rb'
+
+# Controllers
+require_relative 'user_agent_identifier/controllers/base_controller.rb'
+require_relative 'user_agent_identifier/controllers/api_controller.rb'
+
+# Models
+require_relative 'user_agent_identifier/models/result.rb'
